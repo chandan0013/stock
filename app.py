fd3fa8543dc13a4feded8c609430415a528d26e2
Chandan Dhal <chandandhal.er@gmail.com>
2020-06-26 15:15:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/make_plot', methods=['POST'])
 def make_plot():
-  plot = plot_stock_module.plot_stock(stock=request.form['stock'],
+  plot = plot_stock_module.read_pandas(stock=request.form['stock'],
                                    year=request.form['year'],
                                    month=request.form['month'])
   if isinstance(plot, str):
