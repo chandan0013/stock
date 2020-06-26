3b9dad0437988fbd69ef5fe4ada2aa4eecb2ac4b
Chandan Dhal <chandandhal.er@gmail.com>
2020-06-26 15:10:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,29 +12,20 @@ def index():
 
 @app.route('/make_plot', methods=['POST'])
 def make_plot():
-  p = plot_stock_module.plot_stock(stock=request.form['stock'],
+  plot = plot_stock_module.plot_stock(stock=request.form['stock'],
                                    year=request.form['year'],
                                    month=request.form['month'])
-  if isinstance(p, str):
-    # returned error message instead of a plot
-    return p
+  if isinstance(plot, str):
+    return plot
   else:
-    script, div = components(p)
-    return render_template('bokeh_plot.html', script=script, div=div)
+    script, div = components(plot)
+    return render_template('Stock_plot.html', script=script, div=div)
 
 @app.route('/about')
 def about():
   return render_template('about.html')
 
-@app.route('/stocks')
-def stocks():
-  with open('static/WIKI-datasets-codes-sorted.csv', 'r') as f:
-    #content = f.read()
-    return Response(f.read(), mimetype='text/plain')
-
 
 if __name__ == '__main__':
-  #app.run(port=33507)
-  # I'm not sure if the below modifications are necessary, but just in case
   port = int(os.environ.get("PORT", 33507))
   app.run(host='0.0.0.0', port=port)
