cc9b9b2e0e42e60cb6e03c20b8797bc48f9a6ade
Chandan Dhal <chandandhal.er@gmail.com>
2020-06-27 07:00:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,19 +19,15 @@ def read_data(stock='AAPL', year='2017', month='01'):
     try:
       mydata = quandl.get('WIKI/'+stock, start_date=year+'-'+month+'-01', end_date=year+'-12-31')
     except:
-      return f"COULDN'T FIND STOCK {stock}!"
+      return "Company stock not valid"
     
     close_prices = pd.Series()
-    try:
-      if month=='12':
-        close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month)]
-      else:
-        close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month) &
-                                           (mydata.index <  year+'-'+str(int(month)+1))]
-    except:
-      return "COULDN'T FIND DATA IN THAT TIME PERIOD"
+    if month =='12':
+       close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month)]
+    else:
+       close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month) & (mydata.index <  year+'-'+str(int(month)+1))]
   
-    fig1 = figure(title=stock + ' during requested period ', x_axis_label= year + '/' + month, y_axis_label='Reported Stock Price')
+    fig1 = figure(title= 'Requested company closing stock during requested period ', x_axis_label= year + '/' + month, y_axis_label='Reported Stock Price')
     
     fig1.line(pd.DatetimeIndex(close_prices.index.values).day,close_prices.values)
     ##error will also returned to main app.py
