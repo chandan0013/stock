09fa7b2f405cb3c9e7399cfcdd28d0e562f07a16
Chandan Dhal <chandandhal.er@gmail.com>
2020-06-27 06:49:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,40 @@ import os
 from   flask import Flask, render_template, request, redirect, Response
 from   bokeh.embed import components
 
-import read_pandas
+import pandas as pd
+from   bokeh.plotting import figure, output_file, show
+import quandl
+quandl.ApiConfig.api_key = "miwt4s4UiJH7JgU1PA1p"
+
+
+
+
+def read_data(stock='AAPL', year='2017', month='01'):
+
+    stock = stock.upper()
+    
+        
+    mydata = pd.DataFrame()
+    try:
+      mydata = quandl.get('WIKI/'+stock, start_date=year+'-'+month+'-01', end_date=year+'-12-31')
+    except:
+      return f"COULDN'T FIND STOCK {stock}!"
+    
+    close_prices = pd.Series()
+    try:
+      if month=='12':
+        close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month)]
+      else:
+        close_prices = mydata['Close'].loc[(mydata.index >= year+'-'+month) &
+                                           (mydata.index <  year+'-'+str(int(month)+1))]
+    except:
+      return "COULDN'T FIND DATA IN THAT TIME PERIOD"
+  
+    fig1 = figure(title=stock + ' during requested period ', x_axis_label= year + '/' + month, y_axis_label='Reported Stock Price')
+    
+    fig1.line(pd.DatetimeIndex(close_prices.index.values).day,close_prices.values)
+    ##error will also returned to main app.py
+    return fig1
 
 app = Flask(__name__)
 
@@ -12,7 +45,7 @@ def index():
 
 @app.route('/make_plot', methods=['POST'])
 def plot():
-  plot = read_pandas.read_data(stock=request.form['stock'],
+  plot = read_data(stock=request.form['stock'],
                                    year=request.form['year'],
                                    month=request.form['month'])
   if isinstance(plot, str):
